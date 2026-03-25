cfdbf9bb8e16bbaa0d6849cd1b82bc0cac2a847a
mohan4337 <mmenggservice@gmail.com>
2026-03-25 13:00:21 +0530

    modified

--- sun-emirates-frontend/src/App.js
+++ sun-emirates-frontend/src/App.js
@@ -38,7 +38,7 @@ function App() {
   useEffect(() => {
     const password = prompt("Enter Password");
 
-    if (password === "1234") {
+    if (password === "cei@1230") {
       setAllowed(true);
     }
   }, []);
